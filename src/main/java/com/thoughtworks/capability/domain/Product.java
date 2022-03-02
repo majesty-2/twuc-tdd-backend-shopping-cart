18386c57745cd749c233e87603731cf5d11ba52e
Aceysx <aceysx@163.com>
2022-03-02 20:00:49 +0800

    [xinsi]: ProductRepository: refactor:shouldReturnEmptyProducts

--- src/main/java/com/thoughtworks/capability/domain/Product.java
+++ src/main/java/com/thoughtworks/capability/domain/Product.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.capability.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.math.BigDecimal;
 
+@Data
 @Table(name = "product")
 @Entity
 public class Product {
@@ -17,36 +20,4 @@ public class Product {
     private String title;
     private BigDecimal price;
     private Integer quantity;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
