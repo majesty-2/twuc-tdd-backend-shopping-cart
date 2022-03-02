ec2498ffd40e424fd5352593aa9aa908583f7f5f
Aceysx <aceysx@163.com>
2022-03-02 20:34:01 +0800

    [xinsi]: APIShoppingCartControllerTest: test:shouldReturnShoppingCartWhenHasProducts

--- src/test/java/com/thoughtworks/capability/web/api/APIShoppingCartControllerTest.java
+++ src/test/java/com/thoughtworks/capability/web/api/APIShoppingCartControllerTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.capability.web.api;
 import com.thoughtworks.capability.WebApplicationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -23,5 +24,20 @@ public class APIShoppingCartControllerTest extends WebApplicationTest {
             .andExpect(jsonPath("$.products").isArray())
             .andExpect(jsonPath("$.totalAmount").value(BigDecimal.ZERO));
 
+    }
+
+    @Test
+    @Sql("/sql/insert_2_products.sql")
+    public void shouldReturnShoppingCartWhenHasProducts() throws Exception {
+        //given
+        //when
+        mvc.perform(MockMvcRequestBuilders.get("/shoppingCart"))
+            //then
+            .andExpect(jsonPath("$.products").isArray())
+            .andExpect(jsonPath("$.products.[0].id").value(1L))
+            .andExpect(jsonPath("$.products.[0].price").value(BigDecimal.valueOf(20).setScale(1)))
+            .andExpect(jsonPath("$.products.[1].id").value(2L))
+            .andExpect(jsonPath("$.products.[1].price").value(BigDecimal.valueOf(10).setScale(1)))
+            .andExpect(jsonPath("$.totalAmount").value(BigDecimal.valueOf(40).setScale(1)));
     }
 }
