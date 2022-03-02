a8014a23ad160516d842abe4337fa03427018198
Aceysx <aceysx@163.com>
2022-03-02 19:49:20 +0800

    [xinsi]: ShoppingCartService: implementation:shouldReturnEmptyShoppingCartWhenHasNotProducts

--- src/main/java/com/thoughtworks/capability/repository/ProductRepository.java
+++ src/main/java/com/thoughtworks/capability/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package com.thoughtworks.capability.repository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository {
 
-    Object findAll();
+    List findAll();
 }
--- src/main/java/com/thoughtworks/capability/service/ShoppingCartService.java
+++ src/main/java/com/thoughtworks/capability/service/ShoppingCartService.java
@@ -2,15 +2,24 @@ package com.thoughtworks.capability.service;
 
 import com.thoughtworks.capability.repository.ProductRepository;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class ShoppingCartService {
-    public ShoppingCartService(ProductRepository productRepository) {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    public ShoppingCartService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
     public ShoppingCartResponse findShoppingCart() {
-        return null;
+        List products = productRepository.findAll();
+        return new ShoppingCartResponse(products, BigDecimal.ZERO);
     }
 }
