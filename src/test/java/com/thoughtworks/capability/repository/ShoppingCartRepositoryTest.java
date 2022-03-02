9a074c6b3ea136f74d58b305652e3c1144a2e36c
Aceysx <aceysx@163.com>
2022-03-02 20:22:37 +0800

    [xinsi]: ShoppingCartRepositoryTest: test:shouldReturnProducts

--- src/test/java/com/thoughtworks/capability/repository/ShoppingCartRepositoryTest.java
+++ src/test/java/com/thoughtworks/capability/repository/ShoppingCartRepositoryTest.java
@@ -4,7 +4,9 @@ import com.thoughtworks.capability.WebApplicationTest;
 import com.thoughtworks.capability.domain.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,5 +22,19 @@ public class ShoppingCartRepositoryTest extends WebApplicationTest {
         List<Product> products = productRepository.findAll();
         //then
         assertEquals(products.size(), 0);
+    }
+
+    @Test
+    @Sql("/sql/insert_2_products.sql")
+    public void shouldReturnProducts() {
+        //given
+        //when
+        List<Product> products = productRepository.findAll();
+        //then
+        assertEquals(2, products.size());
+        assertEquals(1L, products.get(0).getId());
+        assertEquals(BigDecimal.valueOf(20), products.get(0).getPrice());
+        assertEquals(2L, products.get(0).getId());
+        assertEquals(BigDecimal.valueOf(10), products.get(1).getPrice());
     }
 }
---
+++ src/test/resources/sql/insert_2_products.sql
@@ -0,0 +1,2 @@
+insert into product(id, title, price, quantity) values (1, 'p1', 20, 1);
+insert into product(id, title, price, quantity) values (2, 'p2', 10, 2);
