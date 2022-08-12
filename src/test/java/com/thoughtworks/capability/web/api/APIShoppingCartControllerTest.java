506a7fceabf942881abd924fe9c9d42189a88c5b
Yan Yu <yyan@thoughtworks.com>
2022-08-12 16:11:38 +0800

    feat: return empty shopping cart in api tests

---
+++ src/test/java/com/thoughtworks/capability/web/api/APIShoppingCartControllerTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.capability.web.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class APIShoppingCartControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void should_return_empty_shopping_cart() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/shopping-cart"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.products").isArray())
+                .andExpect(jsonPath("$.products").isEmpty())
+                .andExpect(jsonPath("$.totalPrice").value(BigDecimal.ZERO));
+    }
+}
