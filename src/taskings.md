c48f68c589fe481d364be42f4c0cc99663a389a3
Yan Yu <yyan@thoughtworks.com>
2022-09-19 15:00:10 +0800

    feat: return empty shopping cart in controller

--- src/taskings.md
+++ src/taskings.md
@@ -1,3 +1,6 @@
+
+
+
 - 场景1：Given 购物车中没有商品，When请求购物车，Then返回空的购物车信息和商品总价（数据结构和样例）
 - [ ] Controller:  stub service, 接收请求、调用service，返回购物车信息
 - [ ] Service：stub repository，调用 repository 获取商品，计算总价返回购物车信息
@@ -5,7 +8,7 @@
 - [ ] API 测试：fake DB
 
 ```json
-// GET /shoppingCart
+// GET /shopping-cart
 {
   "response": {
     "products": [
---
+++ src/test/java/com/thoughtworks/capability/web/ShoppingCartControllerTest.java
@@ -0,0 +1,51 @@
+package com.thoughtworks.capability.web;
+
+
+import com.thoughtworks.capability.service.ShoppingCartService;
+import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+
+import static java.util.Collections.emptyList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ShoppingCartControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ShoppingCartService service;
+
+    @Test
+    void should_return_empty_shopping_cart() throws Exception {
+        when(service.getShoppingCart()).thenReturn(new ShoppingCartResponse(emptyList(), BigDecimal.ZERO));
+
+        mvc.perform(MockMvcRequestBuilders.get("/shopping-cart"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.products").isArray())
+                .andExpect(jsonPath("$.products").isEmpty())
+                .andExpect(jsonPath("$.totalPrice").value(BigDecimal.ZERO));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
