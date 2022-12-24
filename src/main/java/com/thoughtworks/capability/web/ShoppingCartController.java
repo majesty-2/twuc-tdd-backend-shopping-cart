b5e5fc32ae48f5849e96d598a8f932d1323ecede
gitqh <quhang19881208@163.com>
2022-12-24 22:29:19 +0800

    refactor: use lombok to simplify class

--- src/main/java/com/thoughtworks/capability/web/ShoppingCartController.java
+++ src/main/java/com/thoughtworks/capability/web/ShoppingCartController.java
@@ -2,19 +2,17 @@ package com.thoughtworks.capability.web;
 
 import com.thoughtworks.capability.service.ShoppingCartService;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/shopping-cart")
+@RequiredArgsConstructor
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
-
-    public ShoppingCartController(final ShoppingCartService shoppingCartService) {
-        this.shoppingCartService = shoppingCartService;
-    }
 
     @GetMapping
     public ShoppingCartResponse fetchShoppingCart() {
--- src/main/java/com/thoughtworks/capability/web/dto/ShoppingCartResponse.java
+++ src/main/java/com/thoughtworks/capability/web/dto/ShoppingCartResponse.java
@@ -2,21 +2,13 @@ package com.thoughtworks.capability.web.dto;
 
 import java.math.BigDecimal;
 import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
 public class ShoppingCartResponse {
     private final List products;
     private final BigDecimal totalAmount;
 
-    public ShoppingCartResponse(final List products, final BigDecimal totalAmount) {
-        this.products = products;
-        this.totalAmount = totalAmount;
-    }
-
-    public List getProducts() {
-        return products;
-    }
-
-    public BigDecimal getTotalAmount() {
-        return totalAmount;
-    }
 }
