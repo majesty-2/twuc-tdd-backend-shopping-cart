fd8c7d3dcb7dc642d5aedcac2c3393c740937b79
gitqh <quhang19881208@163.com>
2022-12-24 23:19:22 +0800

    feat: return 2 products in repository

--- src/test/java/com/thoughtworks/capability/repository/ProductRepositoryTest.java
+++ src/test/java/com/thoughtworks/capability/repository/ProductRepositoryTest.java
@@ -2,11 +2,15 @@ package com.thoughtworks.capability.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@Rollback
 class ProductRepositoryTest {
 
     @Autowired
@@ -22,5 +26,29 @@ class ProductRepositoryTest {
         // then
         assertThat(products).isEmpty();
 
+    }
+
+    @Test
+    @Sql("/sql/insert_2_products.sql")
+    void should_return_2_products() {
+        // given
+
+        // when
+        final var products = productRepository.findAll();
+
+        // then
+        assertThat(products).hasSize(2);
+
+        final var colaProduct = products.get(0);
+        assertThat(colaProduct.getId()).isEqualTo(1L);
+        assertThat(colaProduct.getName()).isEqualTo("cola");
+        assertThat(colaProduct.getPrice()).isEqualByComparingTo(BigDecimal.ONE);
+        assertThat(colaProduct.getQuantity()).isEqualByComparingTo(1);
+
+        final var candyProduct = products.get(1);
+        assertThat(candyProduct.getId()).isEqualTo(2L);
+        assertThat(candyProduct.getName()).isEqualTo("candy");
+        assertThat(candyProduct.getPrice()).isEqualByComparingTo(BigDecimal.valueOf(2));
+        assertThat(candyProduct.getQuantity()).isEqualTo(2);
     }
 }
---
+++ src/test/resources/sql/insert_2_products.sql
@@ -0,0 +1,2 @@
+INSERT INTO `products`(`id`, `name`, `price`, `quantity`) values (1, 'cola', 1, 1);
+INSERT INTO `products`(`id`, `name`, `price`, `quantity`) values (2, 'candy', 2, 2);
